785bd6969a3ffbbee94aaf95be25bc16c0486c8b
Vadym Stukalov <vadym7@ukr.net>
2025-02-06 15:19:34 +0200

    added route

--- app.js
+++ app.js
@@ -36,6 +36,9 @@ app.use(cors());
 app.use(xss());
 
 // routes
+app.use("/", (req, res) => {
+  res.send(`<h1>Jobs API</h1> <a href = "/docs">Documentation</a>`);
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
 
