ac7b38e8428537cd70fe0895923b91a433ba8e78
Vadym Stukalov <vadym7@ukr.net>
2025-02-06 20:46:41 +0200

    add swagger

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const xss = require("xss-clean");
 const cors = require("cors");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -37,8 +43,11 @@ app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
-  res.send(`<h1>Jobs API</h1> <a href = "/docs">Documentation</a>`);
+  res.send(`<h1>Jobs API</h1> <a href = "/api-docs">Documentation</a>`);
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
 
